ed2021c54b5f5a81d3d31c0f2ca2d7465d54865b
gabrielbarross <gabriel.barros@vtex.com.br>
2020-01-14 16:46:05 -0300

    Basic organization created

---
+++ fundamentos/organize.js
@@ -0,0 +1,11 @@
+// LINHA DE CÓDIGO OU SENTENÇA DE CÓDIGO
+console.log("linha de código")
+
+// BLOCO (AGRUPA SENTENÇA DE CÓDIGO)
+{
+    {
+        let top = "top"
+        let mais = "mas esse é mais top"
+        console.log(top + " " + mais)
+    }
+}
