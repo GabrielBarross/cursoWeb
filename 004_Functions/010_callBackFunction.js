13a086925a07903e24720200c4fc1c1de43a4465
GabrielBarross <gabriel.barros@vtex.com.br>
2020-06-30 14:15:57 -0300

    Functions New Lessons

--- 004_Functions/010_callBackFunction.js
+++ 004_Functions/010_callBackFunction.js
@@ -12,12 +12,3 @@ companies.forEach(company => console.log(company))
 
 
 
-// -------- SECOND EXAMPLE ------------
-const grades = [2, 4, 10, 8, 9, 6, 7.5, 6.8, 3]
-let badGrades = []
-for (let i in grades){
-    if(grades[i] < 7){
-        badGrades.push(grades[i])
-    }
-}
-console.log(badGrades)
